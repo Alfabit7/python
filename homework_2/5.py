1183771f8196f5a6c6836cf731ea90aa26d06966
Alfabit7 <tritg@yandex.ru>
2022-12-17 14:56:46 +0300

    adede code with two radnom  list

--- homework_2/5.py
+++ homework_2/5.py
@@ -1,1 +1,20 @@
 # 17 Реализуйте алгоритм перемешивания массива
+import random
+start = 0
+end = 10
+count = end
+list = []
+list2 = []
+for i in range(start, end):
+    list.append(i)
+print(list)
+
+while count > 0:
+    el = random.randint(start, end)
+    if el in list2:
+        continue
+
+    else:
+        list2.append(el)
+        count -= 1
+print(list2)
