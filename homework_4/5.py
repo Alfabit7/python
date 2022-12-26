795b9af4129180b4853f4076d3564da634612449
Alfabit7 <tritg@yandex.ru>
2022-12-26 23:12:57 +0300

    added works code for task5

--- homework_4/5.py
+++ homework_4/5.py
@@ -5,12 +5,16 @@ polinomial_2 = ''
 with open("homework_4/polinomial_1.txt", "r") as file:
     for line in file:
         polinomial_1 = line
+
+print()
 print(polinomial_1)
+print("+")
 
 with open("homework_4/polinomial_2.txt", "r") as file:
     for line in file:
         polinomial_2 = line
 print(polinomial_2)
+print('=')
 
 polinomial_1 = polinomial_1.split('+')
 polinomial_2 = polinomial_2.split('+')
@@ -21,38 +25,25 @@ polinomial_2 = ('').join(polinomial_2).split()
 last_two_charts = 2
 polinomial_1 = polinomial_1[:-last_two_charts]
 polinomial_2 = polinomial_2[:-last_two_charts]
-# ['4x8', '4x7', '24x6', '10x5', '96x4', '52x3', '88x2', '100x', '65']
-# print(f'polinoial_1 = {polinomial_1}')
-# ['80x5', '85x4', '53x3', '86x2', '98x', '55']
-# print(f'polinoial_2 =  {polinomial_2}')
 
-print()
 polinomial_1_str = ('x').join(polinomial_1)
 polinomial_2_str = ('x').join(polinomial_2)
-# print(polinomial_1_str)
-# print(polinomial_2_str)
+
 polinomial_1 = polinomial_1_str.split('x')
 polinomial_2 = polinomial_2_str.split('x')
-# print(f'polinominal_1 = {polinomial_1}')
-# print(f'polinomial_2 = {polinomial_2}')
-print()
+
 list_1 = []
 list_2 = []
-list_3 = []
-list_4 = []
 for i in range(len(polinomial_1)):
     if i % 2 == 0:
         list_1.append(polinomial_1[i])
     else:
-        # list_2.append(polinomial_1[i])
         continue
-
 
 for i in range(len(polinomial_2)):
     if i % 2 == 0:
         list_2.append(polinomial_2[i])
     else:
-        # list_4.append(polinomial_2[i])
         continue
 
 
@@ -68,10 +59,32 @@ else:
 diff = len(max_len_polinomial) - len(min_len_polinomial)
 result_list = max_len_polinomial[:diff]
 max_len_polinomial = max_len_polinomial[diff:]
-print(max_len_polinomial)
-print(min_len_polinomial)
+
 for i in range((len(min_len_polinomial))):
     min_len_polinomial[i] = int(
         min_len_polinomial[i]) + int(max_len_polinomial[i])
-print(min_len_polinomial)
-print(result_list+min_len_polinomial)
+
+result_list = result_list+min_len_polinomial
+
+k = len(result_list)-1
+i = 0
+result_str = ''
+while i != int(len(result_list)-1):
+    if i == 0:
+        result_el = str(result_list[i])+'x**'+str(k)
+        result_str = result_str + '' + result_el
+    elif k == 1:
+        result_el = ' + '+str(result_list[i])+'x'
+        result_str = result_str + '' + result_el
+
+    else:
+        result_el = str(result_list[i])+'x**'+str(k)
+        result_str = result_str + ' + ' + result_el
+    i += 1
+    k -= 1
+
+result_str = result_str+' + ' + str(result_list[len(result_list)-1]) + ' = 0'
+print()
+print(result_str)
+with open("homework_4/sum_polinomial.txt", "w") as somefile:
+    somefile.write(result_str)
