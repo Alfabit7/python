51332f7b40678c4184ae4f82b59128ba512d3b46
Alfabit7 <tritg@yandex.ru>
2022-12-26 23:59:25 +0300

    change code for task_5

--- homework_4/5.py
+++ homework_4/5.py
@@ -1,70 +1,72 @@
 # Даны два файла, в каждом из которых находится запись многочлена. Задача - сформировать файл, содержащий сумму многочленов.
-polinomial_1 = ''
-polinomial_2 = ''
+directory_1 = 'homework_4/polinomial_1.txt'
+directory_2 = 'homework_4/polinomial_2.txt'
 
-with open("homework_4/polinomial_1.txt", "r") as file:
-    for line in file:
-        polinomial_1 = line
+# считываем строку с многочленом из файла
 
+
+def Read_files(directory: str):
+    with open(directory, "r") as file:
+        for line in file:
+            polinomial_1 = line
+            return polinomial_1
+
+
+polinomial_1 = Read_files(directory_1)
+polinomial_2 = Read_files(directory_2)
 print()
 print(polinomial_1)
 print("+")
-
-with open("homework_4/polinomial_2.txt", "r") as file:
-    for line in file:
-        polinomial_2 = line
 print(polinomial_2)
 print('=')
 
-polinomial_1 = polinomial_1.split('+')
-polinomial_2 = polinomial_2.split('+')
-polinomial_1 = ('').join(polinomial_1).split('**')
-polinomial_2 = ('').join(polinomial_2).split('**')
-polinomial_1 = ('').join(polinomial_1).split()
-polinomial_2 = ('').join(polinomial_2).split()
-last_two_charts = 2
-polinomial_1 = polinomial_1[:-last_two_charts]
-polinomial_2 = polinomial_2[:-last_two_charts]
-
-polinomial_1_str = ('x').join(polinomial_1)
-polinomial_2_str = ('x').join(polinomial_2)
-
-polinomial_1 = polinomial_1_str.split('x')
-polinomial_2 = polinomial_2_str.split('x')
-
-list_1 = []
-list_2 = []
-for i in range(len(polinomial_1)):
-    if i % 2 == 0:
-        list_1.append(polinomial_1[i])
-    else:
-        continue
-
-for i in range(len(polinomial_2)):
-    if i % 2 == 0:
-        list_2.append(polinomial_2[i])
-    else:
-        continue
+# Парсим коэффиценты из многочлена и записываем в список
 
 
-len_polinomial_1 = len(list_1)
-len_polinomial_2 = len(list_2)
+def Parsing_ratio(polinomial: str):
+    polinomial = polinomial.split('+')
+    polinomial = ('').join(polinomial).split('**')
+    polinomial = ('').join(polinomial).split()
+    last_two_charts = 2
+    polinomial = polinomial[:-last_two_charts]
+    polinomial_str = ('x').join(polinomial)
+    polinomial = polinomial_str.split('x')
+    temp = []
+    for i in range(len(polinomial)):
+        if i % 2 == 0:
+            temp.append(polinomial[i])
+            # print(list_1)
+        else:
+            continue
+    polinomial = temp.copy()
+    return polinomial
+
+
+polinomial_1 = Parsing_ratio(polinomial_1)
+polinomial_2 = Parsing_ratio(polinomial_2)
+
+# Находим находим длины многочленов и делаем срез с большого многочлена  по длине маленького
+len_polinomial_1 = len(polinomial_1)
+len_polinomial_2 = len(polinomial_2)
 if len_polinomial_1 > len_polinomial_2:
-    max_len_polinomial = list_1
-    min_len_polinomial = list_2
+    max_len_polinomial = polinomial_1
+    min_len_polinomial = polinomial_2
 else:
-    min_len_polinomial = list_1
-    max_len_polinomial = list_2
-
+    min_len_polinomial = polinomial_1
+    max_len_polinomial = polinomial_2
 diff = len(max_len_polinomial) - len(min_len_polinomial)
 result_list = max_len_polinomial[:diff]
 max_len_polinomial = max_len_polinomial[diff:]
 
+# считаем сумму коэффицентов многочленов и записываем в список sum_ratio
+#  после чего склеиваем два списка: первый срез от большого коэффициенты которого не надо было складывать и просуммированный список sum_ratio
+sum_ratio = []
 for i in range((len(min_len_polinomial))):
-    min_len_polinomial[i] = int(
+    sum_ratio.append(i)
+    sum_ratio[i] = int(
         min_len_polinomial[i]) + int(max_len_polinomial[i])
+result_list = result_list+sum_ratio
 
-result_list = result_list+min_len_polinomial
 
 k = len(result_list)-1
 i = 0
@@ -84,7 +86,7 @@ while i != int(len(result_list)-1):
     k -= 1
 
 result_str = result_str+' + ' + str(result_list[len(result_list)-1]) + ' = 0'
-print()
 print(result_str)
+print()
 with open("homework_4/sum_polinomial.txt", "w") as somefile:
     somefile.write(result_str)
