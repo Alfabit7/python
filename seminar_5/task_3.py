4ac1418b089bba457299104a3fef33e38db5cbcc
Alfabit7 <tritg@yandex.ru>
2023-01-13 16:57:33 +0300

    added solution for task_2 task_3

--- seminar_5/task_3.py
+++ seminar_5/task_3.py
@@ -1,2 +1,16 @@
 
 # 38. Напишите программу, удаляющую из текста все слова, содержащие "абв".
+
+
+text = 'Напишите прабвограмму, удаляющую из тексабвта все слова, содержащие "абв".'
+text = text.split()
+
+for element in text:
+    if not 'абв' in element:
+        print(element, end=' ')
+
+
+# РЕШЕНИЕ2
+data = 'аф фыв ыва ываабв, ывадлойц. Оывало абвываф, длоываабв.'
+data = ' '.join(list(filter(lambda slovo: not 'абв' in slovo, data.split())))
+print(data)
