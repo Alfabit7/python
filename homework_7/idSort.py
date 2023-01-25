1f37d69d4cb4427d4c69847b8be4879a098508c0
Alfabit7 <tritg@yandex.ru>
2023-01-26 00:47:21 +0300

    change solution for sorting

--- homework_7/idSort.py
+++ homework_7/idSort.py
@@ -1,22 +1,7 @@
-def sortUserId(data_base):
-    otherDataUserList = []
-    dictUser = {}
-    dictUsers = {}
-    userList = []
-    sortedList = []
-    for i in range(len(data_base)):
-        for j in range(1, len(data_base[i])):
-            fio = data_base[i][0]
-            otherDataUserList.append(data_base[i][j])
-        dictUser = {fio: otherDataUserList}
-        dictUsers.update(dictUser)
-        otherDataUserList = []
-    dictUsers = dict(sorted(dictUsers.items()))
+import view
 
-    for name, data in dictUsers.items():
-        userList.append(name)
-        for el in data:
-            userList.append(el)
-        sortedList.append(userList)
-        userList = []
-    return sortedList
+
+def sortUserId(data_base):
+    data_base = sorted(data_base, key=lambda x: x[0])
+    view.show_database(data_base)
+    return data_base
