3249456a2d720b6fbc93855624500a1b113671fa
andrey <tirtg@yandex.ru>
2023-02-07 18:42:34 +0300

    added folder with homework_9

--- seminar_9/main.py
+++ seminar_9/main.py
@@ -1,100 +1,130 @@
-# 5921040825:AAFL8U5nV65wwV2brIEV1O5A3NbrQGH8SrE
 from telebot import types
 import telebot
-bot = telebot.TeleBot("5921040825:AAFL8U5nV65wwV2brIEV1O5A3NbrQGH8SrE")
-# group_2bot
+import random
+TOKEN=''
+bot = telebot.TeleBot(TOKEN)
+
+candies = 99
+maxStep = 28
+name = ''
+whoseMove = ''
 
 
-@bot.message_handler(commands=["start"])
+@bot.message_handler(commands=['start'])
 def start(message):
-    # bot.send_message(message.chat.id, "/button")
-    bot.send_message(message.chat.id, "Введите имя")
-    bot.register_next_step_handler(message, sentence)
-
-
-def sentence(message):
-    text = message.text
-    bot.send_message(message.chat.id, f"Вас зовут - {text}, хотите сыграть?")
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     but1 = types.KeyboardButton("Да")
-    but2 = types.KeyboardButton("нет")
+    but2 = types.KeyboardButton("Нет")
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     markup.add(but1)
     markup.add(but2)
-    # bot.register_next_step_handler(message, AnswerYes)
+    bot.send_message(
+        message.chat.id, f'Привет! Хочешь сыграть в игру конфеты?', reply_markup=markup)
+    bot.register_next_step_handler(message, PlayOrNotPlay)
 
 
-@bot.message_handler(content_types="text")
-def controller(message):
-    print(message.text)
-    if message.text == "Да":
-        bot.send_message(message.chat.id, "Возьмите конфеты от 1 до 28")
-        bot.register_next_step_handler(message, PlayGame)
-    elif message.text == "нет":
+def PlayOrNotPlay(message):
+    if message.text == 'Да':
+        bot.send_message(message.chat.id, 'Давай сыиграем, как тебя зовут?',
+                         reply_markup=types.ReplyKeyboardRemove())
+        bot.register_next_step_handler(message, UserNameRequest)
+    else:
+        bot.send_message(message.chat.id, 'Жаль! Если захочешь сыграть пиши /start',
+                         reply_markup=types.ReplyKeyboardRemove())
+
+
+def UserNameRequest(message):
+    global name
+    name = message.text
+    but1 = types.KeyboardButton("Да")
+    but2 = types.KeyboardButton("Нет")
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    markup.add(but1)
+    markup.add(but2)
+    bot.send_message(
+        message.chat.id, f'{name} ты знаешь правила игры?', reply_markup=markup)
+    bot.register_next_step_handler(message, QuestionRulesGame)
+
+
+def QuestionRulesGame(message):
+    global name, whoseMove, candies
+    whoseMove = random.choice(['user', 'bot'])
+    if whoseMove == 'user':
+        player = name
+    else:
+        player = 'bot'
+    if message.text == 'Да':
         bot.send_message(
-            message.chat.id, f"выход")
-        button(message)
+            message.chat.id, f'Тогда погнали! {name}  ))\n Всего конфет {candies} Максимально можно взять {maxStep} ')
+        bot.send_message(
+            message.chat.id, f'Жеребьевка показала, что первым ходит {player}')
+        controller(message)
+    else:
+        bot.send_message(
+            message.chat.id, f'{name} правила очень легкие! \n В коробке лежат {candies} конфет\n Каждый игрок, почередно берет конфеты, \n брать можно от 1 штуки до {maxStep} штук \n Побеждает тот игрок, кто заберет последние конфеты \n')
+        bot.send_message(
+            message.chat.id, f'Жеребьевка показала, что первым ходит {player}')
+        controller(message)
 
 
-def PlayGame(message):
-    bot.send_message(message.chat.id, f"Вы взяли {message.text}  конфет")
-    bot.register_next_step_handler(message, nextStep)
+def controller(message):
+    global whoseMove, stepUser, stepBot, player, name, candies
+    if candies > 29:
+        if whoseMove == 'user':
+            player = name
+            bot.send_message(
+                message.chat.id, f'Всего конфет {candies}, {player} возьмите конфеты от 1 до {maxStep}')
+            bot.register_next_step_handler(message, InputUser)
+        else:
+            player = 'bot'
+            bot.send_message(message.chat.id, f' ходит {player}')
+            InputBot(message)
+    else:
+        if candies < 29:
+            if player == name:
+                player = 'bot'
+            else:
+                player == 'bot'
+                player = name
+        bot.send_message(message.chat.id, f'Победил {player}')
+        bot.send_message(
+            message.chat.id, f'Сыграем еще раз? Пиши старт /start')
+        candies = 99
 
 
-def NextStep():
-    print('fdsf')
+def InputUser(message):
+    global candies, player, maxStep, whoseMove, stepBot, stepUser
+    while True:
+        try:
+            stepUser = int(message.text)
+            if 0 < stepUser < 29:
+                candies -= stepUser
+                bot.send_message(
+                    message.chat.id, f' {player} Вы взяли {stepUser} конфет, осталось {candies} конфет')
+                whoseMove = 'bot'
+                controller(message)
+                break
+            else:
+                bot.send_message(
+                    message.chat.id, f'только числа от 1 до 28 введите правильное значение!!! ')
+                bot.register_next_step_handler(message, InputUser)
+                break
+
+        except:
+            bot.send_message(
+                message.chat.id, f'Вводить можно только числа от 1 до 28 введите правиьное значение')
+            bot.register_next_step_handler(message, InputUser)
+            break
 
 
-    # def ShowMenu():
+def InputBot(message):
+    global candies, player, maxStep, whoseMove, stepBot, stepUser, name
+    stepBot = random.randint(1, 29)
+    bot.send_message(
+        message.chat.id, f'{player} взял {stepBot} конфет, осталось {candies-stepBot} ')
+    candies -= stepBot
+    whoseMove = 'user'
+    bot.send_message(message.chat.id, f'Теперь ходит {name}')
+    controller(message)
+
+
 bot.infinity_polling()
-
-# Условие задачи: На столе лежит 2021 конфета. Играют два игрока делая ход друг после друга. Первый ход определяется жеребьёвкой. За один ход можно забрать не более чем 28 конфет. Все конфеты оппонента достаются сделавшему последний ход. Сколько конфет нужно взять первому игроку, чтобы забрать все конфеты у своего конкурента?
-
-# a) Добавьте игру против бота
-
-# b) Подумайте как наделить бота "интеллектом"
-
-# import datetime
-# from telebot import types
-# import telebot
-# # pip install pytelegrambotapi
-
-# bot = telebot.TeleBot("5733303924:AAGgd1P7Aau-9Iyl7YZGAh74SdE9AJUWC5s")
-
-
-# @bot.message_handler(commands=["start"])
-# def start(message):
-#     bot.send_message(message.chat.id, "/button")
-
-
-# @bot.message_handler(commands=["button"])
-# def button(message):
-#     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-#     but1 = types.KeyboardButton("Сделать предложение")
-#     but2 = types.KeyboardButton("узнать время")
-#     markup.add(but1)
-#     markup.add(but2)
-#     bot.send_message(message.chat.id, "Выбери ниже", reply_markup=markup)
-
-
-# @bot.message_handler(content_types="text")
-# def controller(message):
-#     print(message.text)
-#     if message.text == "Сделать предложение":
-#         bot.send_message(message.chat.id, "введи имя фамилию")
-#         bot.register_next_step_handler(message, sentence)
-#     elif message.text == "узнать время":
-#         bot.send_message(
-#             message.chat.id, f"текущее время - {datetime.datetime.now()}")
-#         button(message)
-
-
-# def sentence(message):
-#     text = message.text
-#     surname = text.split()[0]
-#     name = text.split()[1]
-#     bot.send_message(
-#         message.chat.id, f"Вас зовут - {name}, фамилия - {surname}")
-#     button(message)
-
-
-# bot.infinity_polling()
